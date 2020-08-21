b9d58bcdcdedbc5545b3805c74128b338135ec6d
Jesse Brinkley <jbrink28@gmail.com>
2020-08-21 05:08:36 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  my_array = []
 end
 
 def create_an_array
-  
+  sodas = ["Pepsi", "Dr.Pepper", "Mountain Dew", "7UP"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  sodas = ["Pepsi", "Dr.Pepper", "Mountain Dew", "7UP"]
+  sodas << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  sodas = ["Pepsi", "Dr.Pepper", "Mountain Dew", "wow"]
+  sodas.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  sodas = ["Pepsi", "Dr.Pepper", "Mountain Dew", "arrays!"]
+  sodas.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  sodas = ["wow", "Dr.Pepper", "Mountain Dew", "7UP"]
+  sodas.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+sodas = ["Pepsi", "Dr.Pepper", "Mountain Dew", "am"]
+sodas[3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+sodas = ["Pepsi", "wow", "Mountain Dew", "7UP"]
+sodas[-3]
 end
 
 def retrieve_last_element_from_array(array)
-  
+sodas = ["Pepsi", "wow", "Mountain Dew", "arrays!"]
+sodas.last
 end
