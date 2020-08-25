212bb55169a6b7365a1ba1b62189550690ee3327
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-25 03:17:56 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
-end
+  []
+  end
 
 def create_an_array
-  
-end
+  ["camryn", "emma", "river", "ashley"]
+  end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
+
 
 def add_element_to_start_of_array(array, element)
-  
-end
+  array.unshift(element)
+  end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
-end
+  array.shift
+  end
 
 def retrieve_element_from_index(array, index_number)
-  
-end
+  array[index_number]
+  end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  x = array.count
+  array[x-1]
 end
